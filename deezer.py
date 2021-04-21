9b2832d471e8a68eecf12ec60939aa3557efcad7
Yonit Robin <yrobin@umich.edu>
2021-04-21 14:11:26 -0400

    added all comments

--- deezer.py
+++ deezer.py
@@ -1,3 +1,6 @@
+#SI206 Final Project
+#By Yonit Robin
+
 import requests
 import re
 import os 
@@ -17,59 +20,84 @@ def setUpDatabase(db_name):
     return cur, conn
 
 def getInfo(cur, conn):
+    '''Takes in cursor and connection. Returns tuple with all of deezer data
+    '''
+    #select song names and artists from Hot 100 songs table and create a tuple called SongArt
     cur.execute(f'SELECT song_name, artist FROM Hot_100_Songs')
     songArt = cur.fetchall()
+
+    #name empty lists 
     artists = []
     artists_no_featuring = []
     songs = []
     songs_no_spaces = []
+
+    #make a list of all artist names
     for i in songArt:
         artists.append(i[1])
+    
+    #make a list of just the first artist for each song so thatt deezer can fetch the data. Takes away words like 'featuring' or 'duet'
+    #replaces spaces with hyphens so that terms can be searched
     for i in artists:
         if 'Featuring' in i or 'Duet' in i or '&' in i:
             no_featuring = i.split('Featuring')[0]
             artists_no_featuring.append(no_featuring.replace(' ','-'))
         else:
             artists_no_featuring.append(i.replace(' ','-'))
+
+    #make list of song names
     for i in songArt:
         songs.append(i[0])
+    
+    #make list of song names with hyphens instead of spaces
     for i in songs:
         songs_no_spaces.append(i.replace(' ','-'))
     
+    #make list of deezer ranks for each song
     deezer_ranks = []
-
     for song in songs_no_spaces:
+        #call the api on each song
         song_url = base + 'search/track?q={}'.format(song)
         song_data = getReq(song_url)
 
+        #if there is no data, say N/A
         if len(song_data["data"]) == 0 or song_data == None:
             ranks = 'N/A'
 
+        #when there is data, fetch the rank of each song and add it to the list
         ranks = song_data['data'][0]['rank']
         deezer_ranks.append(ranks)
     
+    #make list of fan numbers for each artist
     fan_numbers = []
     for person in artists_no_featuring:
+        #call the api on each artist
         fan_url = base + 'search/artist?q={}'.format(person)
         fan_data = getReq(fan_url)
 
+        #if there is no data, say N/A
         if len(fan_data["data"]) == 0 or fan_data == None:
             fans = 'N/A'
+
+        #when there is data, fetch the number of fans for each artist and add it to the list
         else:
             fans = fan_data['data'][0]['nb_fan']
         fan_numbers.append(fans)
     
+    #puts all data into a tuple 
     DeezData = [(songs[i], artists[i], deezer_ranks[i], fan_numbers[i]) for i in range(0, len(songs))]
-    #print(DeezData)
+    
     return DeezData
         
 def makeDeezerTable(cur):
-    #Creates a table to hold Deezer information
-    cur.execute(f'CREATE TABLE IF NOT EXISTS Deezer (song_id INTEGER PRIMARY KEY, song_name TEXT, artist_name TEXT, songs_deezer_rank INTEGER, artist_fan_number INTEGER)')
+    '''Creates a table to hold all of the deezer information
+    '''
+    cur.execute(f'CREATE TABLE IF NOT EXISTS Deezer (track_id INTEGER PRIMARY KEY, song_name TEXT, artist_name TEXT, songs_deezer_rank INTEGER, artist_fan_number INTEGER)')
 
 def getReq(base):
-    #Takes in the api request and returns in a dictionary format
-    
+    '''Takes in the api request and returns in dictionary format
+    '''
+    #request works if status code is 200
     r = requests.get(base)
     if r.status_code == 200:
         return json.loads(r.content)
@@ -78,65 +106,99 @@ def getReq(base):
 
 
 def fillTable(cur, conn):
-    #Fills up Deezer table
-    
+    '''Takes in curson and connection and fills up the deezer table with the information fetched
+    '''
+    #call the getInfo function
     data = getInfo(cur, conn)
-    song_id = 0
+
+    #initialize track_id and increment value for each song
+    track_id = 0
+
+    #loop thru data and assign each value to a column in the table
     for song in data:
-        song_id += 1
+        track_id += 1
         song_name = song[0]
         artist_name = song[1]
         songs_deezer_rank = song[2]
         artist_fan_number = song[3]
-        cur.execute('INSERT OR IGNORE INTO Deezer (song_id, song_name, artist_name, songs_deezer_rank, artist_fan_number) VALUES (?,?,?,?,?)', (song_id, song_name, artist_name, songs_deezer_rank, artist_fan_number))
+        cur.execute('INSERT OR IGNORE INTO Deezer (track_id, song_name, artist_name, songs_deezer_rank, artist_fan_number) VALUES (?,?,?,?,?)', (track_id, song_name, artist_name, songs_deezer_rank, artist_fan_number))
     conn.commit()
+
+    #code takes a very long time to run, so this prints "done filling table" when the code is done
     print('done filling table')
 
 def avgFans(cur,conn):
-    #average fan numbers
+    '''Takes in cursor and connection, and calculates the average deezer fan number for artists of Billboard's top 100 songs
+    '''
+    #select fan numbers from deezer table
     cur.execute("SELECT artist_fan_number FROM Deezer")
     data = cur.fetchall()
+
+    #make a list of all fan numbers
     fans_list =  [i for t in data for i in t]
+
+    #set all 'N/A's to 0 so that it does not effect the average
     new_lst = [0 if i =='N/A' else i for i in fans_list]
+
+    #turn all fan numbers from strings to integers
     fan_ints = [int(item) for item in new_lst]
+
+    #calculate the average
     all_fans = 0
     for i in fan_ints:
         all_fans += i
     avgFans = all_fans/len(fan_ints)
+
     fanStatement = f"The average fan number for the artists in the 'Hot 100 Songs' chart is {avgFans}"
     return fanStatement
        
 def avgRank(cur, conn):
+    '''Takes in cursor and connection, and calculates the average deezer fan number for Billboard's top 100 songs
+    '''
+    #select song rank from deezer table
     cur.execute("SELECT songs_deezer_rank FROM Deezer")
     data = cur.fetchall()
+
+    #make a list of all ranks
     rank_list =  [i for t in data for i in t]
+
+    #set all 'N/A's to 0 so that it does not effect the average
     new_lst = [0 if i =='N/A' else i for i in rank_list]
+
+    #turn all fan numbers from strings to integers
     rank_ints = [int(item) for item in new_lst]
+
+    #calculate the average
     all_ranks = 0
     for i in rank_ints:
         all_ranks += i
     avgRanks = all_ranks/len(rank_ints)
+
     rankStatement = f"The average deezer rank for the songs in the 'Hot 100 Songs' chart is {avgRanks}"
     return rankStatement
 
 def writeToFile(filename, cur, conn):
-   #Takes in a filename (string) as an input and the database cursor/connection.
-   # Returns nothing. Creates a file and writes return value of the
-   # function average_popularity(), average_length(), average_energy(), and max_popularity() to the file.
+    ''' Takes in the filename, cursor and connection. 
+        Creates a file and writes the values of avgRank() and avgFans() into a txt file
+        Returns nothing
+    '''
     path = os.path.dirname(os.path.abspath(__file__)) + os.sep
+    
+    #open file for writing
     f = open(path + filename, "w")
 
+    #call on the avgRank() and avgFans() functions
     avg_deezer_rank = avgRank(cur, conn)
     avg_fan_number = avgFans(cur, conn)
 
+    #format the txt file
     f.write("Statistics from the 'Deezer' Table: \n\n")
     f.write("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n\n")
     f.write(avg_deezer_rank + "\n\n")
     f.write(avg_fan_number + "\n\n")
-    f.close()
 
-   
-   
+    #close the file
+    f.close()
     
 def main():
     cur, conn = setUpDatabase('top_100_songs.db')
