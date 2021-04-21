c7b15380f3842197d6a285bf096d855afb8d5bbb
Yonit Robin <yrobin@umich.edu>
2021-04-20 20:19:30 -0400

    done filling deezer table

--- deezer.py
+++ deezer.py
@@ -55,6 +55,10 @@ def getInfo(cur, conn):
     for song in songs_no_spaces:
         song_url = base + 'search/track?q={}'.format(song)
         song_data = getReq(song_url)
+
+        if len(song_data["data"]) == 0 or song_data == None:
+            ranks = 'N/A'
+
         ranks = song_data['data'][0]['rank']
         deezer_ranks.append(ranks)
     
@@ -64,14 +68,22 @@ def getInfo(cur, conn):
         fan_data = getReq(fan_url)
 
         if len(fan_data["data"]) == 0 or fan_data == None:
-            continue
-
-        fans = fan_data['data'][0]['nb_fan']
+            fans = 'N/A'
+        else:
+            fans = fan_data['data'][0]['nb_fan']
         fan_numbers.append(fans)
-    #print(len(fan_numbers)) #ONLY HAVE INFO FOR 96 ARTISTS
+    
+        
+        
+        
+    #print(len(songs))
+    #print(len(artists))
+    #print(len(deezer_ranks))
+    #print(len(fan_numbers))
 
     
     DeezData = [(songs[i], artists[i], deezer_ranks[i], fan_numbers[i]) for i in range(0, len(songs))]
+    #print(DeezData)
     return DeezData
         
      
@@ -80,7 +92,6 @@ def getInfo(cur, conn):
 
 def makeDeezerTable(cur):
     #Creates a table to hold Deezer information
-    
     cur.execute(f'CREATE TABLE IF NOT EXISTS Deezer (song_id INTEGER PRIMARY KEY, song_name TEXT, artist_name TEXT, songs_deezer_rank INTEGER, artist_fan_number INTEGER)')
 
 def getReq(base):
@@ -92,70 +103,7 @@ def getReq(base):
     else:
         return None
 
-'''def getArtistList(cur, conn):
-    artists_tup = []
-    cur.execute(f'SELECT artist FROM Hot_100_Songs')
-    for names in cur.fetchall():
-        artists_tup.append(names)
 
-    artist_list = [item for i in artists_tup for item in i]
-    artists_no_spaces = []
-    for i in artist_list:
-        artists_no_spaces.append(i.replace(' ','-'))
-    #replace space with hyphen so that search will work
-    return artists_no_spaces
-'''
-
-    
-    
-
-'''def getDeezData(base, cur, conn):
-    
-     = getSongList(cur, conn)
-    song_ranks = []
-    for rank in song_names:
-        song_url = base + 'search/track?q={}'.format(rank)
-        song_data = getReq(song_url)
-
-        if len(song_data["data"]) == 0 or song_data == None:
-           continue
-
-        song_rank = song_data["data"][0]['rank']
-        song_ranks.append(song_rank)
-    
-    artists = getArtistList(cur, conn)
-    artist_names = []
-    for artist in artists:
-        artist_url = base + 'search/artist?q={}'.format(artist)
-        artist_data = getReq(artist_url)
-        print(artist_url)
-
-        
-            
-        artist = artist_data['data'][0]['name']
-        artist_names.append(artist.replace(' ','-'))
-
-
-    artist_fans = []
-
-    #print(artist_names)
-    #print(len(artist_names))
-    for person in artist_names:
-        fan_url = base + 'search/artist?q={}'.format(person)
-        fan_data = getReq(fan_url)
-        
-        
-        fans = fan_data['data'][0]['nb_fan']
-        artist_fans.append(fans)
-    
-    #DeezData = [(song_names[i], artist_names[i], song_ranks[i], artist_fans[i]) for i in range(0, len(song_names))]
-    #return DeezData
-    print(len(song_names))
-    print(len(artist_names))    
-    print(len(song_ranks))
-    print(len(artist_fans))
-    
- '''   
 def fillTable(cur, conn):
     #Fills up Deezer table
     
@@ -167,20 +115,19 @@ def fillTable(cur, conn):
         artist_name = song[1]
         songs_deezer_rank = song[2]
         artist_fan_number = song[3]
-        cur.execute('INSERT OR IGNORE INTO Deezer (artist_id, song_name, artist_name, songs_deezer_rank, artist_fan_number) VALUES (?,?,?,?,?)', (song_id, song_name, artist_name, songs_deezer_rank, artist_fan_number))
+        cur.execute('INSERT OR IGNORE INTO Deezer (song_id, song_name, artist_name, songs_deezer_rank, artist_fan_number) VALUES (?,?,?,?,?)', (song_id, song_name, artist_name, songs_deezer_rank, artist_fan_number))
     conn.commit()
-    print('done')
+    print('done filling table')
     
 
 def main():
     cur, conn = setUpDatabase('top_100_songs.db')
     getInfo(cur, conn)
-    #makeDeezerTable(cur)
+    makeDeezerTable(cur)
     getReq(base)
-    #getDeezData(artists, artists_no_featuring, songs, songs_no_spaces, base, cur, conn)
+    getInfo(cur, conn)
     fillTable(cur, conn)
-    #getArtistList(cur, conn)
-    #getSongList(cur, conn)
+    
 
 if __name__ == "__main__":
     main()
